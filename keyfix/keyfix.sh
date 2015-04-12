e561a9fefa2b9c915c7e411313e094b37d68d982
Jonathan Yantis <devnull@yantis.net>
2015-04-11 19:59:37 -0700

    Fix for Docker issue #1295 modified:   keyfix.sh

--- keyfix/keyfix.sh
+++ keyfix/keyfix.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# Ran into a problem with the user docker directory being owned by 
+# root in some instances. I think it is related to issue #1295
+chown -R docker /home/docker
+chgrp -R docker /home/docker
+
 # The concept here is to use authorized keys if the user provided them
 # and if they didn't then allow password access.
 
 if [ -f /authorized_keys ]; then
   >&2 echo "Adding authorized keys for root"
   mkdir -p /root/.ssh
-  cp --parents /authorized_keys /root/.ssh/
+  cp /authorized_keys /root/.ssh/
 
   # authorize our user docker
   >&2 echo "Adding authorized keys for user docker"
   mkdir -p /home/docker/.ssh
   cp /authorized_keys /home/docker/.ssh/
-  chown docker /home/docker/.ssh/authorized_keys
-  chgrp users /home/docker/.ssh/authorized_keys
+  chown -R docker /home/docker/.ssh
+  chgrp -R docker /home/docker/.ssh
 else
   >&2 echo "No authorized keys so letting users login with a password."
   sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
